aa5bef053b92bd1fc7879aefcff340525d2b8a69
Kush Raghani <kushraghani@Kushs-MacBook-Air.local>
2025-03-20 16:29:56 +0100

    setting up reservations app and implementing CRUD on it

---
+++ apps/reservations/src/reservations.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ReservationsService } from './reservations.service';
+import { ReservationsController } from './reservations.controller';
+import { DatabaseModule } from '@app/shared';
+import { ConfigModule } from '@nestjs/config';
+import { ReservationsRepository } from './reservations.repository';
+import {
+  ReservationDocument,
+  ReservationSchema,
+} from './models/reservation.schema';
+
+@Module({
+  imports: [
+    DatabaseModule,
+    DatabaseModule.forFeature([
+      {
+        name: ReservationDocument.name,
+        schema: ReservationSchema,
+      },
+    ]),
+    ConfigModule.forRoot(),
+  ],
+  controllers: [ReservationsController],
+  providers: [ReservationsService, ReservationsRepository],
+})
+export class ReservationsModule {}
---
+++ apps/reservations/src/reservations.repository.ts
@@ -0,0 +1,17 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { ReservationDocument } from './models/reservation.schema';
+import { AbstractRepository } from '@app/shared';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class ReservationsRepository extends AbstractRepository<ReservationDocument> {
+  protected readonly logger = new Logger(ReservationsRepository.name);
+
+  constructor(
+    @InjectModel(ReservationDocument.name)
+    reservationModel: Model<ReservationDocument>,
+  ) {
+    super(reservationModel);
+  }
+}
